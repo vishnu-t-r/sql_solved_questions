644f36df8c0ee800009d8f340df60d15380114ed
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-02-08 23:19:02 +0530

    questions (basic-intermediate-advanced)
    10 questions each

---
+++ 93_questions (basic-intermediate-advanced).sql
@@ -0,0 +1,46 @@
+--what is the difference between delete, drop and truncate?
+--when is group by used?
+--difference between where clause and having cluase?
+--what are scalare and aggregate functions in sql? examples?
+--what does coalesce function do?
+--what is the difference between SQL and NoSQL databases?
+--What is a primary key and foreign key?
+--How do you add a new record to a table?
+--What is the purpose of the SQL LIKE clause?
+--How can you retrieve unique values from a table using SQL?
+
+--Bonus questions
+	--Explain the concept of ACID properties in the context of databases.
+	--How do you update data in a table using SQL?
+
+--what are constraints? how many constraints are there?
+--how many primary key and unique key can be there in a table?
+--what are joins in sql? type of joins?
+--difference between union and union all?
+--what is the order of execution in sql code?
+--how many joins are required to join 8 tables?
+--What is the difference between a candidate key, a primary key, and a unique key?
+--Explain the purpose of the SQL CASE statement with an example?
+--What is a subquery, and how is it different from a JOIN?
+--What is a stored procedure and when would you use one?
+--What is the purpose of the SQL CHECK constraint?
+
+
+--what are window functions or analytical functions?
+--what is the difference between rank and dense_rank?
+--what is view? how is it different from a table?
+--what is cte? where to use cte?
+--what is the difference between sql vs plsql?
+--where to apply cross join?
+--How do you optimize a SQL query for better performance (4 techniques)?
+--What is the purpose of the SQL PIVOT and UNPIVOT operators?
+--What are the advantages and disadvantages of using NoSQL databases compared to SQL databases?
+--What is the purpose of the SQL LEAD() and LAG() window functions?
+
+
+--how to remove duplicates in a table?
+--how to get top 5 sales in highest sales?
+--how to display alternate rows from a table?
+--write a query to fetch the employee and manager name from the same table?
+
+
