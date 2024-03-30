e2420e4eaabc05cfe11ee8be538fe018ba1385ec
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-03-30 23:05:58 +0530

    Flags per video
    flags per video (stratascratch)

---
+++ 113_Flags per video (stratascratch).sql
@@ -0,0 +1,18 @@
+--Flag per video
+
+use int_ques;
+
+/*
+For each video, find how many unique users flagged it. 
+A unique user can be identified using the combination of their first name and last name. 
+Do not consider rows in which there is no flag ID.
+*/
+
+--select * from user_flags;
+
+select video_id,
+        count(distinct (concat(user_firstname,user_lastname))) as user_count
+from user_flags
+where flag_id is not null
+group by video_id
+
