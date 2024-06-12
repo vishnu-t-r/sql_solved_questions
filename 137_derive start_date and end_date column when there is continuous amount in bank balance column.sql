d99e728a89efd725d5bc6f1f14d672d27dbaf011
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-06-12 23:09:10 +0530

    derive start_date and end_date column
    derive start_date and end_date column when there is continuous amount in bank balance column

---
+++ 137_derive start_date and end_date column when there is continuous amount in bank balance column.sql
@@ -0,0 +1,56 @@
+use sql_challenge;
+
+/*
+Write SQL to derive Start_Date and End_Date column when there is
+continuous amount in Balance column as shown below.
+*/
+
+/*
+create table bank_account(
+amount int,
+date_stamp date
+)
+
+insert into bank_account(amount, date_stamp)
+values(26000,'2099-01-01'),
+(26000,'2099-01-02'),
+(26000,'2099-01-03'),
+(30000,'2099-01-04'),
+(30000,'2099-01-05'),
+(26000,'2099-01-06'),
+(26000,'2099-01-07'),
+(32000,'2099-01-08'),
+(31000,'2099-01-09')
+*/
+
+--select * from bank_account;
+
+with t1 as(
+select amount, date_stamp,
+		row_number() over(partition by amount order by date_stamp asc) as flag
+from bank_account
+--order by date_stamp asc
+),
+t2 as(
+select amount,
+		date_stamp,
+		flag,
+		day(date_stamp) as date_day,
+		(day(date_stamp)-flag) as flag_diff
+from t1
+--order by date_stamp asc
+),
+t3 as(
+select amount,
+		date_stamp,
+		flag,
+		date_day,
+		flag_diff,
+		min(date_stamp) over(partition by amount,flag_diff) as bank_start_date,
+		max(date_stamp) over(partition by amount,flag_diff) as bank_end_date
+from t2
+--order by date_stamp asc
+)
+select distinct amount, bank_start_date, bank_end_date
+from t3
+order by bank_start_date asc
