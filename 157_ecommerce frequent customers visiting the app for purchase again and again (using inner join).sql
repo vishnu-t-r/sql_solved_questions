b03eff7005ef00b4f90a15dbe5892bc911bba088
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-08-13 22:21:56 +0530

    ecommerce frequent customers-method 2
    ecommerce frequent customers visiting the app for purchase again and again

---
+++ 157_ecommerce frequent customers visiting the app for purchase again and again (using inner join).sql
@@ -0,0 +1,31 @@
+use sql_challenge;
+
+
+/*
+Question :- Ecommerce company is trying to identify returning active users. 
+A returning active user is a user that has made a second purchase within 7 days of any other of their purchases.
+Write a SQL query to display the list of Userld of these returning active users.
+*/
+
+with active_customer as (
+select t1.userId as user_1,
+		t1.createdat as createdat_1,
+		t2.userId as user_2,
+		t2.createdat as createdat_2,
+		datediff(day,t1.createdat,t2.createdat) as diff_date
+from Transactions_Ecomm t1
+inner join Transactions_Ecomm t2 
+on t1.userId = t2.userId and abs(datediff(day,t1.createdat,t2.createdat)) <= 7 
+--where abs(datediff(day,t1.createdat,t2.createdat)) <> 0
+						
+)
+select distinct user_1
+from active_customer
+where abs(datediff(day,createdat_1,createdat_2)) <> 0
+
+/*
+select * from Transactions_Ecomm
+--where userid in (110,129)
+order by userid asc, createdat asc
+*/
+
